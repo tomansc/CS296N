a36cd99e98fcf0f810ae24f5e8d041cf892e1747
tomansc <samantha.toman@gmail.com>
2016-01-26 02:56:01 -0800

    lab3 initial

---
+++ Toman296Lab3/Toman296Lab2/Controllers/ForumViewController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Toman296Lab2.Models;
+
+namespace Toman296Lab2.Controllers
+{
+    public class ForumViewController : Controller
+    {
+        private Toman296Lab2Context db = new Toman296Lab2Context();
+
+        // GET: ForumView
+        public ActionResult Index() // I'm currently experimenting with this default method to ensure that 
+            // *all* components of the ForumView appear here. Still to do: Add Member for each message, fix
+            // ForumView view so that it appears a lot more "forum-like."
+        {
+            var fview = db.ForumViews.ToList()[0];
+            fview.Messages = db.Message.ToList();
+            
+            //return View(db.ForumViews.ToList());
+            return View(fview);
+        }
+
+        // GET: ForumView/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ForumView forumView = db.ForumViews.Find(id);
+            if (forumView == null)
+            {
+                return HttpNotFound();
+            }
+            return View(forumView);
+        }
+
+        // GET: ForumView/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ForumView/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ForumID,ForumName,LastModified")] ForumView forumView)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ForumViews.Add(forumView);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(forumView);
+        }
+
+        // GET: ForumView/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ForumView forumView = db.ForumViews.Find(id);
+            if (forumView == null)
+            {
+                return HttpNotFound();
+            }
+            return View(forumView);
+        }
+
+        // POST: ForumView/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ForumID,ForumName,LastModified")] ForumView forumView)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(forumView).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(forumView);
+        }
+
+        // GET: ForumView/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ForumView forumView = db.ForumViews.Find(id);
+            if (forumView == null)
+            {
+                return HttpNotFound();
+            }
+            return View(forumView);
+        }
+
+        // POST: ForumView/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ForumView forumView = db.ForumViews.Find(id);
+            db.ForumViews.Remove(forumView);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Toman296Lab3/Toman296Lab2/Models/Message.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Toman296Lab2.Models
+{
+    public class Message
+    {
+        private List<string> topics = new List<string>();
+        public int MessageID { get; set; }
+        public string Subject { get; set; }
+        public Member PostingMember { get; set; }
+        public DateTime Date { get; set; }
+        public string Body { get; set; }
+        public List<string> Topics { 
+            get 
+            { 
+                return topics; 
+            } 
+            set 
+            { 
+                topics = value; 
+            } 
+        }
+        // Ultimately, there would be
+        // some validation here to ensure that only certain values specified by
+        // a faraway reference table in the source DB would make it into this 
+        // list. For the purpose of a view, however, it doesn't seem necessary 
+        // to make a Topic class. That feels overwrought to me. The front-end
+        // could still sort by topics without resorting to a Topic class. 
+    }
+}
